0f3fb3923f6eeb132864e6c644d521cdf13a32d0
Oscar Ortiz <ortizg.oscar@gmail.com>
2020-10-06 09:15:19 -0300

    new problems

---
+++ leet_codes/1470_Shuffle_the_array/shuffle.py
@@ -0,0 +1,25 @@
+"""
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+"""
+nums = [1,2,3,4,4,3,2,1]
+
+n = int(len(nums)/2)
+
+# Attempt 1
+"""
+Runtime: 44 ms, faster than 88.71% of Python online submissions for Shuffle the Array.
+Memory Usage: 13.8 MB, less than 5.36% of Python online submissions for Shuffle the Array.
+"""
+shuffled_list =[]
+for i  in range(len(nums)):
+    if (i < n):
+        shuffled_list.append(nums[i])
+        shuffled_list.append(nums[n+i])
+        i+=1
+
+
+print('\n\n==printing results==\n\n')
+print(nums)
+print(shuffled_list)
