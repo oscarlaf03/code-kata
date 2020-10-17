0a7656ac912ab3370e5a1c68c88c54a31a818e70
Oscar Ortiz <ortizg.oscar@gmail.com>
2020-10-17 13:18:21 -0300

    workign

--- katas/challenge_2/run.py
+++
@@ -1,8 +0,0 @@
-from FraudDetector import FraudDetector
-
-detector = FraudDetector()
-
-detector.process_all_transactions()
-detector.save_processed_transactions()
-frauds = detector.get_fraudulent_transactions()
-print('fraud details:\n', frauds)
